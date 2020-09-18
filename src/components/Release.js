b427168cb59b7b31e72d5639a21637f6566a5e28
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-09-18 18:28:54 +0200

    format releases

--- src/components/Release.js
+++ src/components/Release.js
@@ -2,27 +2,19 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import Box from "@material-ui/core/Box";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    minWidth: 275,
-    marginTop: 12,
-  },
   card: {
     display: "flex",
     flexDirection: "column-reverse",
     padding: 12,
+    minWidth: 275,
+    marginTop: 12,
     [theme.breakpoints.up('sm')]: {
       flexDirection: "row",
       justifyContent: "space-between",
     }
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
   },
   title: {
     fontSize: 16,
@@ -38,23 +30,17 @@ const useStyles = makeStyles((theme) => ({
 
 const Release = ({ release }) => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent className={classes.card}>
-        <Typography className={classes.title} variant="button">
-          {bull} {release.title}
+    <Card className={classes.card} variant="outlined">
+      <Typography className={classes.title} variant="button">
+        {release.title}
+      </Typography>
+      <Box className={classes.date}>
+        <Typography className={classes.pos}  color="textSecondary">
+          Release date: {release.date}
         </Typography>
-        <Box className={classes.date}>
-          <Typography className={classes.pos}  color="textSecondary">
-            Release date: 
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {release.date}
-          </Typography>
-        </Box>
-      </CardContent>
+      </Box>
     </Card>
   );
 };
